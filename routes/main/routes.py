1d262aeb346bbcd99d1c15aae9f8a792985df2f8
AlanShelbyCode <121756992+AlanShelbyCode@users.noreply.github.com>
2023-10-18 09:09:57 +0300

    Update routes.py

--- routes/main/routes.py
+++ routes/main/routes.py
@@ -5,11 +5,3 @@ from flask import render_template, request, redirect
 @app.route('/', methods=["GET"] )
 def index():
     return render_template('index.html')
-
-
-# @app.route("/")
-# def main():
-#     articles = Article.query.all()
-#     categories = Category.query.all()
-#     return render_template("main/index.html", articles=articles, categories=categories)
-#
